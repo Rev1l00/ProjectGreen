842cd62f7b1e97bde418891079704ae7c10ffe90
Rev1l00 <Oliverric@viken.no>
2023-09-28 15:18:03 +0200

    dawdaw

--- Assets/Scripts/SAButton.cs
+++ Assets/Scripts/SAButton.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class SAButton : MonoBehaviour
 {
+    public GameObject SouthAmericaImage;
     void Start()
     {
-        this.GetComponent<SouthAmerica>().alphaHitTestMinimumThreshold = 0.1f;
+        // this.GetComponent<SouthAmerica>().alphaHitTestMinimumThreshold = 0.1f;
     }
 }
