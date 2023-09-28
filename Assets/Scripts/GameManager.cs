e5b8cf9da7ffda6b2fa2c46487658384873d69ba
KevinViken <kevinwes@viken.no>
2023-09-28 15:11:09 +0200

    .

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,36 +7,30 @@ public class GameManager : MonoBehaviour
     public void EU_button()
     {
         Debug.Log("Europe Pressed");
-        
     }
 
     public void AF_button()
     {
         Debug.Log("Africa Pressed");
-        
     }
 
     public void AS_button()
     {
         Debug.Log("Asia Pressed");
-        
     }
 
     public void NA_button()
     {
         Debug.Log("North America Pressed");
-        
     }
 
     public void SA_button()
     {
         Debug.Log("South America Pressed");
-        
     }
 
     public void AU_button()
     {
         Debug.Log("Australia Pressed");
-        
     }
 }
