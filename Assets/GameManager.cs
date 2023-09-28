3492ae37b1b9586309c11a2b6ea0a4354aa19e38
KevinViken <kevinwes@viken.no>
2023-09-28 11:08:06 +0200

    ny

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void EU_button()
+    {
+        Debug.Log("Europe Pressed");
+        
+    }
+
+    public void AF_button()
+    {
+        Debug.Log("Africa Pressed");
+        
+    }
+}
---
+++ Assets/Scripts/EuButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuButton : MonoBehaviour
+{
+    public void pressed()
+    {
+        Debug.Log("trykket");
+        
+    }
+}
