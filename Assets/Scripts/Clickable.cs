65d45b1e5353d6891fa97a372dc8840bba96afee
KevinViken <kevinwes@viken.no>
2023-11-02 08:53:45 +0100

    panel when click

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -5,14 +5,24 @@ using UnityEngine.UI;
 
 public class Clickable : MonoBehaviour
 {
+    public Image panelImage;
     public float alphaThreshold = 0.1f;
     public SquarePlacement squarePlacement; // Reference to the SquarePlacement script.
 
     void Start()
     {
         this.GetComponent<Image>().alphaHitTestMinimumThreshold = alphaThreshold;
-
         squarePlacement = GameObject.Find("SquarePlacementController").GetComponent<SquarePlacement>();
+    
+        panelImage = GameObject.Find("ContInf").GetComponentInChildren<Image>();
+            if (panelImage != null)
+            {
+                panelImage.enabled = false; // Ensure the image is initially hidden
+            }
+            else
+            {
+                Debug.LogError("Image component not found in the parent GameObject. Make sure it exists.");
+            }
     }
 
     public void buttn_pressed()
@@ -20,6 +30,8 @@ public class Clickable : MonoBehaviour
         if (squarePlacement.inPlacementMode == false)
         {
             Debug.Log(this + " was pressed!");
+
+            panelImage.enabled = !panelImage.enabled;
             // Add any other functionality you want to perform when clicking.
         }
     }
