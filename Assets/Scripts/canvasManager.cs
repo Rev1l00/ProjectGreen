2a825c1be5f97ce26c3a673386257854aa7a3e6e
Rev1l00 <Oliverric@viken.no>
2024-01-18 14:35:03 +0100

    jkggjy

--- Assets/Scripts/canvasManager.cs
+++ Assets/Scripts/canvasManager.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using TMPro;
 
 public class canvasManager : MonoBehaviour
 {
     public GameObject contButtons;
+    public TMP_Text continentText;
+
+    public string selectedCont = "None";
 
     public void disableContButtons()
     {
         contButtons.SetActive(false);
-        Debug.Log("Ocean was pressed");
     }
+
+    public void btn_pressed(string buttonName)
+    {
+        contButtons.SetActive(false);   // Disable the buttons
+        selectedCont = buttonName;       // Set selectedCont to the name of the pressed button
+        continentText.SetText(selectedCont);    // Set the continentText
+        contButtons.SetActive(true);    // Enable the buttons
+    }
+
 }
